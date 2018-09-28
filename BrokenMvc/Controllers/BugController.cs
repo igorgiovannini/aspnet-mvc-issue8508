a75dec71d78ef65134f426558f44d7dbda6db0c9
Igor Giovannini <igor.giovannini@post.ch>
2018-09-28 17:58:29 +0200

    Example for not working File() method in ASP.NET Core MVC

---
+++ BrokenMvc/Controllers/BugController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+
+namespace BrokenMvc.Controllers
+{
+    public class BugController : Controller
+    {
+        private readonly string _imagePath;
+
+        public BugController(IHostingEnvironment hostingEnvironment) {
+            var imagePath = System.IO.Path.Combine(hostingEnvironment.WebRootPath, "images", "Microsoft-logo_rgb_wht.png");
+            var provider = new PhysicalFileProvider(hostingEnvironment.WebRootPath);
+            _imagePath = provider.GetFileInfo("images/Microsoft-logo_rgb_wht.png").PhysicalPath;
+        }
+
+        public IActionResult NotWorking()
+        {
+            return File(_imagePath, "image/png");
+        }
+
+        public IActionResult Working()
+        {
+            return File(System.IO.File.ReadAllBytes(_imagePath), "image/png");
+        }
+    }
+}
